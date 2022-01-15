fad4db086958b9c6f858578c653544c50b28667c
zentred <87871486+zentred@users.noreply.github.com>
2022-01-15 00:40:34 +0000

    Update t.py

--- t.py
+++ t.py
@@ -1,4 +1,4 @@
-import requests, json, re, time
+import requests, json, re, time, threading
 req = requests.Session()
 
 with open("config.json", "r") as config:
@@ -9,26 +9,29 @@ webhook = config['webhook']
 discordid = config['discord_id']
 req.cookies['.ROBLOSECURITY'] = cookie
 already = []
+values = None
 
 def rolimons():
-    ids = []
-    information = []
-    r = requests.get('https://www.rolimons.com/deals').text
-    items = re.findall('item_details = (.*?);', r)[0]
-    total = json.loads(items)
-    for x in total:
-        info = total[x]
-        if info[5] == None:
-            name, use, projected, itemid = info[0], info[2], info[4], x
-            ids.append(itemid)
-            information.append(f'{use}/{name}/{projected}')
-        elif info[5] != None:
-            name, use, projected, itemid = info[0], info[5], info[4], x
-            ids.append(itemid)
-            information.append(f'{use}/{name}/{projected}')
-    c = zip(ids, information)
-    values = dict(c)
-    return values
+    global values
+    while True:
+        ids = []
+        information = []
+        r = requests.get('https://www.rolimons.com/deals').text
+        items = re.findall('item_details = (.*?);', r)[0]
+        total = json.loads(items)
+        for x in total:
+            info = total[x]
+            if info[5] == None:
+                name, use, projected, itemid = info[0], info[2], info[4], x
+                ids.append(itemid)
+                information.append(f'{use}/{name}/{projected}')
+            elif info[5] != None:
+                name, use, projected, itemid = info[0], info[5], info[4], x
+                ids.append(itemid)
+                information.append(f'{use}/{name}/{projected}')
+        c = zip(ids, information)
+        values = dict(c)
+        time.sleep(600)
 
 def overall():
     global already
@@ -38,11 +41,16 @@ def overall():
 
 def trades():
     all = []
-    r = req.get('https://trades.roblox.com/v1/trades/completed?cursor=&limit=25&sortOrder=Desc').json()
-    for x in r['data']:
-        if not x['id'] in already:
-            all.append(x['id'])
-    check(all)
+    while True:
+        try:
+            r = req.get('https://trades.roblox.com/v1/trades/completed?cursor=&limit=25&sortOrder=Desc').json()
+            if 'data' in r:
+                for x in r['data']:
+                    if not x['id'] in already:
+                        all.append(x['id'])
+                check(all)
+                break
+        except: pass
 
 def check(all):
     global already
@@ -74,24 +82,34 @@ def check(all):
             if len(str(percentage).split('.')[1]) >= 3:
                 percentage = round(percentage, 2)
 
+        their_username = r['offers'][1]['user']['name']
+        their_id = r['offers'][1]['user']['id']
+
         data = {
             'content': f'<@{discordid}>',
             'embeds':[{
+                'author': {
+                    'name': f'Trade from {their_username}\n\u200b',
+                    'url': f'https://www.roblox.com/users/{str(their_id)}/profile'
+                    },
                 'color': int('00FF00',16),
                 'fields': [
                     {'name': f'📤 Gave [{me}]','value': f'{me_hook}','inline':True},
                     {'name': f'\u200b','value': f'\u200b','inline':True},
                     {'name': f'📥 Received: [{them}]','value': f'{them_hook}','inline':True},
                     {'name': '💸 Profit','value': f'{profit} ({percentage}%)','inline':False},
-                    ]
+                    ],
+                    'thumbnail': {
+                        'url': f'http://www.roblox.com/Thumbs/Avatar.ashx?x=200&y=200&Format=Png&username={their_username}',
+                        }
                 }]
             }
         requests.post(webhook, json=data)
         already.append(tid)
 
 
-values = rolimons()
+threading.Thread(target=rolimons).start()
 overall()
 while True:
     trades()
-    time.sleep(300)
+    time.sleep(120)
